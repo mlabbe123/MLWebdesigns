cb2dda27fd9128bb3f975299331fd81d74b00ef8
Mathieu Labbé <mathieu.labbe@hotmail.com>
2014-09-06 20:39:39 -0400

    Fichiers ordonnés

---
+++ src/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+
+#Folder settings
+relative_assets = true			#because we're not working from the root
+css_dir = "css"          		#where the CSS will saved
+sass_dir = "sass"           	#where our .scss files are
+images_dir = "images"    		#the folder with your images
+bg_images_dir = "css/bg_images"	#the folder with background images
+fonts_dir = "css/fonts"			#the folder with fonts files
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+# Obviously
+preferred_syntax = :scss
